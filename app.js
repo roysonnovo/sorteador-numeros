fda0af156b6991b63012bf110a4ad55228847165
roysonnovo <royson.novo@gmail.com>
2025-09-01 10:17:24 -0300

    Função Sortear

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear(){
+    let qtd = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`Quantidade: ${qtd}`);
+    alert(`Do número: ${de}`);
+    alert(`Até: ${ate}`);
+}
