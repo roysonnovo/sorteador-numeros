79e9487be0204756569270d7db8110e110899d24
roysonnovo <royson.novo@gmail.com>
2025-09-01 18:16:27 -0300

    Função Obter Número

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 function sortear(){
     let qtd = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
-    alert(`Quantidade: ${qtd}`);
-    alert(`Do número: ${de}`);
-    alert(`Até: ${ate}`);
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i <  qtd; i++) {
+        numero = obtNum(de, ate);
+        sorteados.push(numero);
+    }
+    alert(sorteados);
+}
+
+function obtNum(min,max){
+
+return Math.floor(Math.random() * (max - min + 1)) + min;
+
 }
